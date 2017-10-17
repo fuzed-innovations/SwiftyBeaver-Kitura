551d6666daa921f9c5b9f6706df296dbb25ea6aa
ryanpeggs <ryanpeggs@users.noreply.github.com>
2017-10-17 18:43:05 +0100

    Point swift <4 package dependency at FUZED fork

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftyBeaverKitura",
     dependencies: [
-        .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", majorVersion: 1),
+        .Package(url: "https://github.com/fuzed-innovations/SwiftyBeaver.git", majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion:1, minor: 7)
     ]
 )
